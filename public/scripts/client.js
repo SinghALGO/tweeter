d654db7e1c87dab3d266f698575b2cab52602fec
SinghALGO <karanssandhu92@gmail.com>
2023-12-06 15:05:56 -0800

    Add: createTweetElement function added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,23 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(() => {
+  const tweetData = {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  };
+  const createTweetElement = function (tweet) {
+    const $tweet = $(
+      `<article><header><div class="header-left"><img src="${tweet.user.avatars}" alt="user-avatar" /><p>${tweet.user.name}</p></div><p>${tweet.user.handle}</p></header><h4>${tweet.content.text}</h4><footer><p class="footer-left">${tweet.created_at}</p><div class="footer-right"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div></footer></article>`
+    );
 
+    return $tweet;
+  };
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+  $("#tweets-container").append($tweet);
+});
