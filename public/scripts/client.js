a4513a7857ea09326dae7f34cfc4b89486ef0506
SinghALGO <karanssandhu92@gmail.com>
2023-12-06 16:47:33 -0800

    Add: Form submission using Jquery added:

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,6 @@ $(document).ready(() => {
 
     return $tweet;
   };
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
   const renderTweets = function (tweets) {
     for (const tweet of tweets) {
@@ -37,5 +13,16 @@ $(document).ready(() => {
     }
   };
 
-  renderTweets(data);
+  $("#tweet-form").submit((event) => {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: $("#tweet-form").serialize(),
+      success: () => {
+        $("#tweet-form")[0].reset();
+        $(".counter").text(140);
+      },
+    });
+  });
 });
